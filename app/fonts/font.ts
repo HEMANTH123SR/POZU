913cb62df865e34f42bd9102561383d77c391c52
hemanth <applicationcomputer257@gmail.com>
2024-09-23 19:07:54 +0530

    add font files and font related code

---
+++ app/fonts/font.ts
@@ -0,0 +1,26 @@
+import localFont from "next/font/local";
+
+export const apercuBold = localFont({
+  src: "../fonts/apercu-bold.woff",
+  display: "swap",
+});
+
+export const apercuRegular = localFont({
+  src: "../fonts/apercu-regular.woff",
+  display: "swap",
+});
+
+export const cutAlong = localFont({
+  src: "../fonts/cut.otf",
+  display: "swap",
+});
+
+export const panchoMedium = localFont({
+  src: "../fonts/pancho-medium.woff2",
+  display: "swap",
+});
+
+export const panchoSemibold = localFont({
+  src: "../fonts/pancho-semibold.woff2",
+  display: "swap",
+});
