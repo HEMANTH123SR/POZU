5b857d91146008d382bfd3a14a94863541e29edd
hemanth <applicationcomputer257@gmail.com>
2024-09-26 00:27:35 +0530

    content and code improvemtn on top product section

--- components/sections/top-product-section.tsx
+++ components/sections/top-product-section.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Link from "next/link";
-import { panchoSemibold } from "@/app/fonts/font";
-
+import { panchoSemibold, panchoMedium } from "@/app/fonts/font";
 
 import {
   topSellingPetFoodProducts,
@@ -12,12 +11,18 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export function TopSellingProduct() {
   return (
-    <section className="w-screen h-auto flex flex-col space-y-14">
+    <section className="w-screen h-auto flex flex-col justify-center items-center space-y-20 my-20">
+      <h4
+        className={`text-6xl text-blue-900 w-[90vw] ${panchoSemibold.className} `}
+      >
+        Best Selling Pet Products
+      </h4>
+
       {/* pet food */}
-      <div className="w-screen h-auto flex flex-col space-y-6 justify-center items-center py-28">
+      <div className="w-screen h-auto flex flex-col space-y-6 justify-center items-center">
         <div className="w-[90vw] flex flex-col space-y-2 justify-start ">
-          <h4 className={`${panchoSemibold.className} text-lg text-blue-900 `}>
-            Best Selling Pet Products Related To Food
+          <h4 className={`${panchoMedium.className} text-2xl text-blue-900 `}>
+            Best Selling Pet Products On Food Cateogory
           </h4>
           <Link
             href={"/food"}
@@ -31,7 +36,7 @@ export function TopSellingProduct() {
             {topSellingPetFoodProducts.map((product, index) => (
               <div
                 key={index}
-                className="shrink-0 flex flex-col justify-center items-center space-y-3 w-72 h-auto p-4 py-8 border rounded-xl"
+                className="shrink-0 flex flex-col justify-center items-center space-y-3 w-72 min-h-96 h-auto p-4 py-8 border rounded-xl"
               >
                 <img
                   className="w-full h-auto "
@@ -49,9 +54,9 @@ export function TopSellingProduct() {
       </div>
 
       {/* pet store */}
-      <div className="w-screen h-auto flex flex-col space-y-6 justify-center items-center py-28">
-        <div className="w-[90vw] flex justify-start ">
-          <h4 className={`${panchoSemibold.className} text-lg text-blue-900 `}>
+      <div className="w-screen h-auto flex flex-col space-y-6 justify-center items-center ">
+        <div className="w-[90vw] flex flex-col justify-start ">
+          <h4 className={`${panchoMedium.className} text-2xl text-blue-900 `}>
             Best Selling Pet Products Related To Food
           </h4>
           <Link
