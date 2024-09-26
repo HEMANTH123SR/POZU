482adf5446f96edb65c93cfe9ed25b0865425da6
hemanth <applicationcomputer257@gmail.com>
2024-09-26 10:03:28 +0530

    add faq section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { panchoSemibold } from "@/app/fonts/font";
 import { DogSliderComponent } from "@/components/sections/dog-slider";
 import { TopSellingProduct } from "@/components/sections/top-product-section";
 import { FeatureSection } from "@/components/sections/feature-section";
-
+import { FaqSection } from "@/components/sections/faq-section";
 export default function Home() {
   return (
     <div className="h-full flex flex-col overflow-x-hidden">
@@ -35,6 +35,7 @@ export default function Home() {
       <FeatureSection />
       <DogSliderComponent />
       <TopSellingProduct />
+      <FaqSection />
     </div>
   );
 }
